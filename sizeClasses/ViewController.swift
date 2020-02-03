ca4cda982e445c333facb63cf7d42da34f252516
Cao Mai <caomai@rocketmail.com>
2020-02-02 23:56:18 -0800

    first commit

---
+++ sizeClasses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sizeClasses
+//
+//  Created by Cao Mai on 2/2/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
